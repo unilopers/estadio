4aa5bb974dbb5556b914bd81e316641d348c48d3
arthur7con <158327083+arthur7con@users.noreply.github.com>
2025-11-25 20:22:01 -0300

    :sparkles: add UsuarioFuncionario

---
+++ estadio-main/src/main/java/grupo_1/estadio/repository/UsuarioFuncionarioRepository.java
@@ -0,0 +1,6 @@
+package grupo_1.estadio.repository;
+
+import grupo_1.estadio.model.UsuarioFuncionario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsuarioFuncionarioRepository extends JpaRepository<UsuarioFuncionario, Integer> {}
