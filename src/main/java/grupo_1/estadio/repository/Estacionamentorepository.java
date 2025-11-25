3104386def683c2ca1cb961098c9d08e2d6e3014
Joao <joaofegar@gmail.com>
2025-11-25 15:09:09 -0300

    Adiciona estrutura MVC do estacionamento

---
+++ src/main/java/grupo_1/estadio/repository/Estacionamentorepository.java
@@ -0,0 +1,8 @@
+package grupo_1.estadio.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import grupo_1.estadio.model.Estacionamento;
+
+public interface Estacionamentorepository extends JpaRepository<Estacionamento, Long> {
+
+}
