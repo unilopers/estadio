719c184d298bff8e92b30258145fb2a01559dc84
viniciustofano <vinicius.then@gmail.com>
2025-11-25 23:18:32 -0300

    :recycle: Refatorar controller de Assento para melhorar organização e validações

--- AssentoController.java
+++ AssentoController.java
@@ -3,31 +3,74 @@ import java.util.List;
 
 public class AssentoController {
 
-    private AssentoDAO dao;
+    private final AssentoDAO assentoDAO;
 
     public AssentoController(Connection conn) {
-        this.dao = new AssentoDAO(conn);
+        this.assentoDAO = new AssentoDAO(conn);
     }
 
-    public void criarAssento(int idAssento, int idSetor) throws Exception {
-        Assento a = new Assento(idAssento, idSetor);
-        dao.inserir(a);
+    public boolean criarAssento(int idAssento, int idSetor) {
+        try {
+            // regra simples: garantir que setor não seja negativo
+            if (idSetor <= 0) {
+                System.out.println("Setor inválido!");
+                return false;
+            }
+
+            Assento novo = new Assento(idAssento, idSetor);
+            assentoDAO.inserir(novo);
+
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("Erro ao criar assento: " + e.getMessage());
+            return false;
+        }
     }
 
-    public Assento buscarAssento(int id) throws Exception {
-        return dao.buscarPorId(id);
+    public Assento buscarAssento(int idAssento) {
+        try {
+            return assentoDAO.buscarPorId(idAssento);
+        } catch (Exception e) {
+            System.out.println("Erro ao buscar assento: " + e.getMessage());
+            return null;
+        }
     }
 
-    public List<Assento> listarAssentos() throws Exception {
-        return dao.listar();
+    public List<Assento> listarAssentos() {
+        try {
+            return assentoDAO.listar();
+        } catch (Exception e) {
+            System.out.println("Erro ao listar assentos: " + e.getMessage());
+            return null;
+        }
     }
 
-    public void atualizarAssento(int idAssento, int novoSetor) throws Exception {
-        Assento a = new Assento(idAssento, novoSetor);
-        dao.atualizar(a);
+    public boolean atualizarAssento(int idAssento, int novoSetor) {
+        try {
+            if (novoSetor <= 0) {
+                System.out.println("Setor inválido!");
+                return false;
+            }
+
+            Assento atualizado = new Assento(idAssento, novoSetor);
+            assentoDAO.atualizar(atualizado);
+
+            return true;
+
+        } catch (Exception e) {
+            System.out.println("Erro ao atualizar assento: " + e.getMessage());
+            return false;
+        }
     }
 
-    public void deletarAssento(int idAssento) throws Exception {
-        dao.deletar(idAssento);
+    public boolean deletarAssento(int idAssento) {
+        try {
+            assentoDAO.deletar(idAssento);
+            return true;
+        } catch (Exception e) {
+            System.out.println("Erro ao deletar assento: " + e.getMessage());
+            return false;
+        }
     }
 }
