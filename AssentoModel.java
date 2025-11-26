75112b95887c27687d2a0da5ce8bead96d63156c
viniciustofano <vinicius.then@gmail.com>
2025-11-25 23:20:45 -0300

    :recycle: Refatorar model Assento adicionando validações e melhorias estruturais
    Added validation for seat and sector IDs in setters.

--- AssentoModel.java
+++ AssentoModel.java
@@ -1,12 +1,16 @@
+/**
+ * Representa um assento dentro de um setor do estádio.
+ */
 public class Assento {
+
     private int idAssento;
     private int idSetor;
 
     public Assento() {}
 
     public Assento(int idAssento, int idSetor) {
-        this.idAssento = idAssento;
-        this.idSetor = idSetor;
+        setIdAssento(idAssento);
+        setIdSetor(idSetor);
     }
 
     public int getIdAssento() {
@@ -14,6 +18,9 @@ public class Assento {
     }
 
     public void setIdAssento(int idAssento) {
+        if (idAssento <= 0) {
+            throw new IllegalArgumentException("ID do assento deve ser maior que zero.");
+        }
         this.idAssento = idAssento;
     }
 
@@ -22,6 +29,17 @@ public class Assento {
     }
 
     public void setIdSetor(int idSetor) {
+        if (idSetor <= 0) {
+            throw new IllegalArgumentException("ID do setor deve ser maior que zero.");
+        }
         this.idSetor = idSetor;
+    }
+
+    @Override
+    public String toString() {
+        return "Assento{" +
+                "idAssento=" + idAssento +
+                ", idSetor=" + idSetor +
+                '}';
     }
 }
