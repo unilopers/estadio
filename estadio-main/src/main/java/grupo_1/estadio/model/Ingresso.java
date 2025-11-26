648297052a35a09e0af87759e5fee7f3edca7db9
bevilaquapg <pedrobevilaquasilva@gmail.com>
2025-11-25 23:23:35 -0300

    :sparkles: Adiciona Ingresso Model (Entidade) e camada Repository

---
+++ estadio-main/src/main/java/grupo_1/estadio/model/Ingresso.java
@@ -0,0 +1,20 @@
+package group_1.estadio;
+
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "ingressos")
+public class Ingresso {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String codigoIngresso;
+    private BigDecimal precoVenda;
+    private LocalDateTime dataEmissao;
+    private Boolean isUtilizado = false;
+
+}
--- estadio-main/src/main/java/grupo_1/estadio/repository/UsuarioFuncionarioRepository.java
+++ estadio-main/src/main/java/grupo_1/estadio/repository/UsuarioFuncionarioRepository.java
@@ -1,6 +1,8 @@
 package grupo_1.estadio.repository;
 
+import grupo_1.estadio.model.Ingresso;
 import grupo_1.estadio.model.UsuarioFuncionario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.pringframework.stereotype.Repository;
 
 public interface UsuarioFuncionarioRepository extends JpaRepository<UsuarioFuncionario, Integer> {}
